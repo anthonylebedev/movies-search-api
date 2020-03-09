a1aebac940a2350db6e6b1675f7b01383a3b4369
Anthony Lebedev <anthony.lebedev@gmail.com>
2020-03-09 14:09:54 +0300

    promise added

--- script.js
+++ script.js
@@ -5,33 +5,48 @@ function apiSearch(event) {
     event.preventDefault();
     const searchText = document.querySelector('.form-control').value;
     const server = 'https://api.themoviedb.org/3/search/movie?api_key=b71638a04e0b286b616599cdf999587c&language=ru-RU&page=1&include_adult=true&query=' + searchText;
-    console.log(requestApi('GET', server));
+    movie.innerHTML = 'Загрузка...';
+    requestApi('GET', server)
+        .then(function(result) {
+            const output = JSON.parse(result);
+
+            let inner = '';
+    
+            output.results.forEach(function(item) {
+                let nameItem = item.name || item.title;
+                inner += `<div class="col-3">${nameItem}</div>`;            
+            });
+    
+            movie.innerHTML = inner;    
+        })
+        .catch(function( ) {
+            movie.innerHTML = 'Упс...';
+        })
+        ;
 }
 
 searchForm.addEventListener('submit', apiSearch);
 
 function requestApi(method, url) {
-    const request = new XMLHttpRequest();
-    request.open(method, url);
-    request.send();
+    return new Promise (function (resolve, reject) {
+        const request = new XMLHttpRequest();
+        request.open(method, url);
 
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState !== 4) {
-            return;
-        }
-
-        if (request.status !== 200) {
-            console.log(request.status);
-        }
-
-        const output = JSON.parse(request.responseText);
-        let inner = '';
-
-        output.results.forEach(function(item) {
-            let nameItem = item.name || item.title;
-            inner += `<div class="col-3">${nameItem}</div>`;            
+        request.addEventListener('load', function() {
+            if (request.status !== 200) {
+                reject({
+                    status: request.status
+                });        
+            }
+            resolve(request.response);
         });
 
-        movie.innerHTML = inner;
-    });
+        request.addEventListener('error', function() {
+            reject({
+                status: request.status
+            });        
+        });
+
+        request.send();
+    });   
 }
